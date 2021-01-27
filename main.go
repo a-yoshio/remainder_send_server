0d5bace8d46f5bcf9ea4baab4b3331891ae0f1cd
a_yoshino <atsuko.yoshino@rakus.co.jp>
2021-01-27 09:55:37 +0900

    メッセージ配信機能を実装

--- main.go
+++ main.go
@@ -6,19 +6,39 @@ import (
 	"log"
 
 	firebase "firebase.google.com/go"
+	"firebase.google.com/go/messaging"
 )
 
 func sendToToken(app *firebase.App) {
 	ctx := context.Background()
-	_, err := app.Messaging(ctx)
+	client, err := app.Messaging(ctx)
 	if err != nil {
 		log.Fatalf("error getting Messaging client: %v\n", err)
 	}
-	fmt.Println("OK!!")
+	// This registration token comes from the client FCM SDKs.
+	registrationToken := ""
+
+	// See documentation on defining a message payload.
+	message := &messaging.Message{
+		Data: map[string]string{
+			"title":    "Remainder",
+			"contents": "起きる時間だよ！",
+			"tag":      "家",
+		},
+		Token: registrationToken,
+	}
+
+	// Send a message to the device corresponding to the provided
+	// registration token.
+	response, err := client.Send(ctx, message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Response is a message ID string.
+	fmt.Println("Successfully sent message:", response)
 }
 
 func main() { // 最初に実行されるmain()関数を定義
-	// opt := option.WithCredentialsFile("remainder-send-server-2ca730bad69e.json")
 	app, err := firebase.NewApp(context.Background(), nil)
 	if err != nil {
 		fmt.Errorf("ERROR!", err)
